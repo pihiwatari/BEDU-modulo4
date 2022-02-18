fa44ff156aa29407fde40a322c091671d9c093e5
Elias Rayas <eliasrayasg@gmail.com>
2022-02-18 15:40:23 -0600

    Reto 3 WIP

--- session-2/https.js
+++ session-2/https.js
@@ -4,18 +4,14 @@ function getAPOD() {
     .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", (resp) => {
       let data = "";
 
-      resp.setEncoding("utf8");
-      //trozos de información recibida
       resp.on("data", (chunk) => {
         data += chunk;
       });
 
-      // La respuesta completa ha sido recibida. Imprime el resultado
       resp.on("end", () => {
         let body = JSON.parse(data);
-        console.log(" Título:", body.explanation);
-        console.log("\n Explicación", body.explanation);
-        console.log("\n URL:", body.url);
+        console.log(body.url);
+        return body;
       });
     })
     .on("error", (err) => {
@@ -23,4 +19,6 @@ function getAPOD() {
     });
 }
 
-getAPOD();
+const imageURL = getAPOD();
+
+module.exports = imageURL;
---
+++ session-2/save_image.js
@@ -0,0 +1,11 @@
+const https = require("https");
+const fs = require("fs");
+const imageURL = require("./https");
+
+console.log(imageURL);
+
+function downloadImage(url, filepath, callback) {
+  // fs
+  //save image
+  //console.log success | error
+}
